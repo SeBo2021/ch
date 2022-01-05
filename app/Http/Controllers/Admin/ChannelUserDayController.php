2ed6a59cb07b6d708f417b2c61b8b8f246cec378
zhaosi <zhaosi@git.com>
2022-01-05 20:14:52 +0800

    1

--- app/Http/Controllers/Admin/ChannelUserDayController.php
+++ app/Http/Controllers/Admin/ChannelUserDayController.php
@@ -2,17 +2,74 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Models\ChannelCpa;
 use App\Models\ChannelCps;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
+use JetBrains\PhpStorm\Pure;
 
 class ChannelUserDayController extends BaseCurlController
 {
+    public $channelInfo;
+
     public function setModel(): ChannelCps
     {
-        return $this->model = new ChannelCps();
+        $this->channelInfo = DB::connection('origin_mysql')->table('channels')->where('number',admin('account'))->first();
+        $type = $this->channelInfo ? $this->channelInfo->type : 2;
+        return match ($type) {
+            0 => $this->model = new ChannelCpa(),
+            2 => $this->model = new ChannelCps(),
+        };
     }
 
-    public function indexCols(): array
+    public function getCpaIndexCols()
+    {
+        return [
+            [
+                'type' => 'checkbox'
+            ],
+            [
+                'field' => 'name',
+                'minWidth' => 100,
+                'title' => '渠道名称',
+                'align' => 'center'
+            ],
+            [
+                'field' => 'channel_code',
+                'minWidth' => 80,
+                'title' => '渠道码',
+//                'hide' => true,
+                'align' => 'center',
+            ],
+
+            [
+                'field' => 'downloads',
+                'minWidth' => 80,
+                'title' => '今日下载人数',
+                'align' => 'center',
+            ],
+            [
+                'field' => 'unit_price',
+                'minWidth' => 80,
+                'title' => '单价(¥)',
+                'align' => 'center',
+            ],
+            [
+                'field' => 'settlement_amount',
+                'minWidth' => 80,
+                'title' => '结算金额(¥)',
+                'align' => 'center',
+            ],
+            [
+                'field' => 'at_time',
+                'minWidth' => 150,
+                'title' => '统计日期',
+                'align' => 'center'
+            ],
+        ];
+    }
+
+    public function getCpsIndexCols(): array
     {
         return [
             [
@@ -78,6 +135,16 @@ class ChannelUserDayController extends BaseCurlController
         ];
     }
 
+    #[Pure] public function indexCols(): array
+    {
+        $type = $this->channelInfo ? $this->channelInfo->type : 2;
+        return match ($type) {
+            0 => $this->getCpaIndexCols(),
+            2 => $this->getCpsIndexCols(),
+        };
+//        return $this->channelInfo->type;
+    }
+
     public function handleResultModel($model)
     {
         $parentChannelNumber = admin('account');
@@ -106,8 +173,22 @@ class ChannelUserDayController extends BaseCurlController
 
     public function setListOutputItemExtend($item)
     {
-        $item->share_amount = number_format($item->share_amount, 2, '.', '');
-        $item->share_ratio = $item->share_ratio . '%';
+        switch ($this->channelInfo->type) {
+            case 2:
+                $item->share_amount = number_format($item->share_amount, 2, '.', '');
+                $item->share_ratio = $item->share_ratio . '%';
+                break;
+            case 0:
+                $info = DB::connection('origin_mysql')->table('channel')->where('id',$item->channel_id)->first();
+                $item->name = $info->name;
+                $item->channel_code = $info->number;
+                $item->downloads = $info->install;
+                $item->unit_price = $info->unit_price;
+                $item->settlement_amount = round($info->unit_price * $info->install,2);
+                $item->at_time =  date('Y-m-d',$info->created_at);
+                break;
+        }
+
         return $item;
     }
 
---
+++ app/Models/ChannelCpa.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+class ChannelCpa extends BaseModel
+{
+    protected $connection = 'origin_mysql';
+
+    protected $table = 'statistic_day_channel';
+}
