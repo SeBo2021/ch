42757b7943f2e544f505112b1507a2c22f7dc7fa
zhaosi <zhaosi@git.com>
2022-01-27 19:51:00 +0800

    1

--- app/Http/Controllers/Admin/ChannelUserDayController.php
+++ app/Http/Controllers/Admin/ChannelUserDayController.php
@@ -197,10 +197,9 @@ class ChannelUserDayController extends BaseCurlController
                 foreach ($result as $res){
                     if(($res->channel_id==$this->channelInfo->id) || ($res->pid==$this->channelInfo->id)){
                         $channelInfo = $channelsBuild->where('id',$res->channel_id)->first();
-//                        $res->install = (int)round($res->install/100);
-                        $channelInfo->unit_price = $channelInfo->unit_price??0;
+                        $res->install = (int)round($res->install/100);
                         $res->settlement_amount = round($channelInfo->unit_price * $res->install,2);
-//                        $res->unit_price = $channelInfo->unit_price;
+                        $res->unit_price = $channelInfo->unit_price ?? 0;
                         $res->name = $channelInfo->name;
                         $res->number = $channelInfo->number;
                         if(isset($handleLists[$res->channel_id])){
